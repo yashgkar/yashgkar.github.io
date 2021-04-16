09392fefb75ba9354c0baeebc7d04090009af115
Prafful Patil <yashgarudkar@gmail.com>
2021-04-17 00:14:50 +0530

    updated folio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './components/header'
+import Home from './containers/index'
+
+function App() {
+	return (
+		<main>
+			<Header />
+			<Home />
+		</main>
+	)
+}
+
+export default App
