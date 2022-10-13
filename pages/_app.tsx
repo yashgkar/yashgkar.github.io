9176393aba76465229f5770b8415a178441baa2b
Yash Garudkar <yashgarudkar@gmail.com>
2022-10-14 00:07:54 +0530

    fix: gtag revert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,8 @@
 import "../styles/globals.scss"
 import type { AppProps } from "next/app"
-import Script from "next/script"
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<>
-			<Script
-				async
-				src={`https://www.googletagmanager.com/gtag/js?id=G-0VXKZBHJB8`}
-			/>
-			<Script id="google-analytics" strategy="afterInteractive" crossOrigin="use-credentials">
-				{`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-0VXKZBHJB8');
-        `}
-			</Script>
-			<Component {...pageProps} />
-		</>
-	)
+	return <Component {...pageProps} />
 }
 
 export default MyApp
